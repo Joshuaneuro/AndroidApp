81f6086c3ad42b2fa7327bb4936c1465c7dc3de4
Joshuaneuro <joshuavdmeij@gmail.com>
2019-06-28 14:31:49 +0200

    small fixes

--- app/src/main/java/com/example/joshua/androidweek5/LoginFragment.java
+++ app/src/main/java/com/example/joshua/androidweek5/LoginFragment.java
@@ -6,12 +6,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup viewGroup, Bundle savedInstance) {
-        Button loginbutton = (Button) findViewById(R.id.LoginButton);
+        Button loginbutton = (Button) getView().findViewById(R.id.LoginButton);
+        TextView usernameText = getView().findViewById(R.id.loginText);
+        TextView passwordText = getView().findViewById(R.id.passwordText);
+
         return inflater.inflate(R.layout.fragment_login, viewGroup,false);
 
     }
@@ -22,6 +26,8 @@ public class LoginFragment extends Fragment {
 
 
     }
+
+
 
     //TextView textView = (TextView)findViewById(R.id.loginText);
 }
